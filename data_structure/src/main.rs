da2796511360e971da7671264a6de2aa7eae6fe7
petern <petermrdat09@gmail.com>
2022-11-18 12:35:50 +0700

    The first Rust HelloWorld

---
+++ data_structure/src/main.rs
@@ -0,0 +1,21 @@
+mod struct_demo;
+mod sheep_struct;
+mod Arc_run;
+mod some_demo;
+
+mod daily_problem;
+
+
+
+// #[derive(Debug)]
+fn main() {
+    // println!("Hello, world!");
+    // struct_demo::demo()
+    // sheep_struct::main();
+    // Arc::main()
+    // some_demo::demo_whileloop()
+
+    daily_problem::main();
+
+}
+
